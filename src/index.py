779ea78c28f004fafe40c9a934abeeadba85640f
Guilherme Alves Carneiro Borges <tqi_gcarneiro>
2022-04-11 12:04:16 -0300

    reestruturacao do projeto

--- src/index.py
+++ src/index.py
@@ -5,21 +5,20 @@ def connect():
     return psycopg2.connect(host="localhost", database="escola", user="postgres", password="root")
 
 
-def execute():
+def lambda_handler(event, context):
     try:
         print('Connecting')
+        # id_aluno = event['id_aluno']
+        # nome = event['nome']
+        id_aluno = 22
+        nome = 'Vicente Araújo'
         con = connect()
         cur = con.cursor()
-        cur.execute('select * from aluno')
-        result = cur.fetchall()
-        dados = []
-        for dado in result:
-            dados.append(dado)
-
+        cur.execute('insert into aluno values(%s,%s)', (id_aluno, nome))
+        con.commit()
         con.close()
-        print(dados)
     except(Exception, psycopg2.DatabaseError) as error:
         print(error)
 
 
-execute()
+lambda_handler(None, None)
